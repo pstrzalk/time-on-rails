3a62f69b54174f37a6514d9bbdfe14dd258b396a
Paweł Strzałkowski <p.strzalkowski@visuality.pl>
2025-03-16 23:03:33 +0100

    Add current user restriction to reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,6 +6,9 @@ class ReportsController < ApplicationController
     start_date = Date.parse("01/#{@month}/#{@year}")
     end_date = start_date.at_end_of_month
 
-    @reports = Work.select("works.*, SUM(duration) as total_duration").where(date: [ start_date..end_date ]).group(:project_id)
+    @reports = Work.where(user: current_user)
+                   .where(date: [ start_date..end_date ])
+                   .select("works.*, SUM(duration) as total_duration")
+                   .group(:project_id)
   end
 end
