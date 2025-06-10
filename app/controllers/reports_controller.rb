8eefb8da6b72e179084621e2baaa7180dde8c5b2
Paweł Strzałkowski <p.strzalkowski@visuality.pl>
2025-06-10 17:20:12 +0200

    add reports months

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,9 @@
 class ReportsController < ApplicationController
   def index
-    @month = params[:month].presence || DateTime.current.month
-    @year = params[:year] || DateTime.current.year
+    @month = (params[:month].presence || DateTime.current.month).to_i
+    @year = (params[:year].presence || DateTime.current.year).to_i
 
-    start_date = Date.parse("01/#{@month}/#{@year}")
+    start_date = Date.new(@year, @month, 1)
     end_date = start_date.at_end_of_month
 
     @reports = Work.where(user: current_user)
